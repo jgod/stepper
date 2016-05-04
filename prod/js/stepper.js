a810cee3aa695fd54aea35fbb3af8c953fe435e9
Justin Godesky <3506872+jgod@users.noreply.github.com>
2016-05-04 16:37:54 -0400

    Updates: - Added repo field to package.json - Fixed bugs dealing with manual input and the buttons reflecting their state - Cleaned up impl. by hiding some stuff outside public interface

--- prod/js/stepper.js
+++ prod/js/stepper.js
@@ -3,54 +3,46 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+function getNum(str) {
+  return parseInt(str, 10);
+};
+
+function onClick(amount, buttons, field) {
+  if (amount === 0) return;
+  if (!field.validity.valid || field.value === '') field.value = 0;
+
+  if (amount < 0 && getNum(field.value) <= getNum(field.min)) return;else if (amount > 0 && getNum(field.value) >= getNum(field.max)) return;
+
+  field.value = getNum(field.value) + amount;
+  checkValidity(buttons, field);
+};
+
+function checkValidity(buttons, field) {
+  // Disable increments that surpass thresholds.
+  if (field.validity.valid) {
+    buttons.neg.disabled = getNum(field.value) <= getNum(field.min);
+    buttons.pos.disabled = getNum(field.value) >= getNum(field.max);
+  } else {
+    field.value = 0;
+  }
+};
+
 exports.default = {
-  getNum: function getNum(str) {
-    return parseInt(str, 10);
-  },
-
-  cb: function cb(neg, pos, field) {
-    // Disable increments that surpass thresholds.
-    if (field.validity.valid) {
-      if (this.getNum(field.value) <= this.getNum(field.min)) {
-        neg.disabled = true;
-        return;
-      } else if (this.getNum(field.value) >= this.getNum(field.max)) {
-        pos.disabled = true;
-        return;
-      }
-    } else {
-      field.value = 0;
-    }
-
-    pos.disabled = false;
-    neg.disabled = false;
-  },
-
-  bindButton: function bindButton(el, amount, otherEl, field) {
-    el.addEventListener('click', function () {
-      if (amount === 0) return;
-      if (!field.validity.valid || field.value === '') field.value = 0;
-
-      if (amount < 0 && this.getNum(field.value) <= this.getNum(field.min)) return;else if (amount > 0 && this.getNum(field.value) >= this.getNum(field.max)) return;
-
-      field.value = this.getNum(field.value) + amount;
-      this.cb(amount < 0 ? el : otherEl, amount < 0 ? otherEl : el, field);
-    }.bind(this));
-  },
-
   bindAll: function bindAll(opts) {
     opts = opts || {};
     var steppers = document.getElementsByClassName(opts.stepper || 'stepper');
+
     for (var i = 0; i < steppers.length; i++) {
       var pos = steppers[i].getElementsByClassName(opts.add || 'add')[0];
       var neg = steppers[i].getElementsByClassName(opts.subtract || 'subtract')[0];
       var field = steppers[i].getElementsByClassName(opts.field || 'field')[0];
 
-      field.addEventListener('change', function () {
-        this.cb(neg, pos, field);
-      }.bind(this));
-      this.bindButton(pos, 1, neg, field);
-      this.bindButton(neg, -1, pos, field);
+      // Bind field
+      field.addEventListener('change', checkValidity.bind(this, { neg: neg, pos: pos }, field));
+
+      // Bind buttons
+      neg.addEventListener('click', onClick.bind(this, -1, { neg: neg, pos: pos }, field));
+      pos.addEventListener('click', onClick.bind(this, 1, { neg: neg, pos: pos }, field));
     }
   }
 };
